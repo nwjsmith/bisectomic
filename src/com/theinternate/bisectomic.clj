cb2199d6dd93c6a3e586269a94f22251223a32ee
Nate Smith <nate@theinternate.com>
2023-08-17 23:01:28 -0400

    One less global

--- src/com/theinternate/bisectomic.clj
+++ src/com/theinternate/bisectomic.clj
@@ -1,6 +1,14 @@
 (ns com.theinternate.bisectomic
   (:require [datomic.client.api :as d]))
 
-(def client (d/client {:server-type :dev-local :storage-dir :mem :system "dev"}))
+(def client
+  (d/client {:server-type :dev-local :storage-dir :mem :system "dev"}))
 
-(def conn (d/connect client {:db-name "bisectomic"}))
+(defn set-up
+  [client]
+  (let [arg-map {:db-name "bisectomic"}]
+    (d/create-database client arg-map)
+    (d/connect client arg-map)))
+
+(comment
+  (set-up client))
