12730621ed5b4fa6aa27236b91b106679c35c911
Nate Smith <nate@theinternate.com>
2023-08-18 23:42:33 -0400

    Set up the schema as well

--- src/com/theinternate/bisectomic.clj
+++ src/com/theinternate/bisectomic.clj
@@ -4,11 +4,14 @@
 (def client
   (d/client {:server-type :dev-local :storage-dir :mem :system "dev"}))
 
+(def schema
+  [{:db/ident :security/id
+    :db/valueType :db.type/uuid
+    :db/cardinality :db.cardinality/one
+    :db/doc "The unique identifier of the security."}])
+
 (defn set-up
   [client]
   (let [arg-map {:db-name "bisectomic"}]
     (d/create-database client arg-map)
-    (d/connect client arg-map)))
-
-(comment
-  (set-up client))
+    (d/transact (d/connect client arg-map) {:tx-data schema})))
